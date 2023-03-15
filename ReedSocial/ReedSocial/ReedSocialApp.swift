e8e0a1ba23d27eb1c596965962eb7979802556c7
Sima Nerush <2002ssn@gmail.com>
2023-03-15 15:53:45 -0400

    initial

---
+++ ReedSocial/ReedSocial/ReedSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReedSocialApp.swift
+//  ReedSocial
+//
+//  Created by Sima Nerush on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ReedSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
