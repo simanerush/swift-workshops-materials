914e71a4704b4f7ad3262b5213a92ad2c64af711
Sima Nerush <2002ssn@gmail.com>
2023-03-15 16:54:11 -0400

    add photo picker

--- ReedSocial/ReedSocial/ProfileDetailView.swift
+++ ReedSocial/ReedSocial/ProfileDetailView.swift
@@ -6,15 +6,38 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ProfileDetailView: View {
   @Binding var text: String
-    var body: some View {
-      VStack {
-        Image(systemName: "person")
-        TextField("", text: $text)
-          .textFieldStyle(.roundedBorder)
-          .padding(.horizontal, 70)
+  @Binding var image: Image
+  
+  @State var imageItem: PhotosPickerItem?
+  
+  var body: some View {
+    VStack {
+      PhotosPicker("select your profile pic!", selection: $imageItem, matching: .images)
+      image
+        .resizable()
+        .scaledToFill()
+        .frame(width: 200, height: 200)
+        .clipShape(Circle())
+        .padding(8)
+      TextField("", text: $text)
+        .textFieldStyle(.roundedBorder)
+        .padding(.horizontal, 70)
+      
+    }
+    .onChange(of: imageItem) { _ in
+      Task {
+        if let data = try? await imageItem?.loadTransferable(type: Data.self) {
+          if let uiImage = UIImage(data: data) {
+            image = Image(uiImage: uiImage)
+            return
+          }
+        }
+        fatalError("failed to pick the photo")
       }
     }
+  }
 }
--- ReedSocial/ReedSocial/ProfileView.swift
+++ ReedSocial/ReedSocial/ProfileView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ProfileView: View {
-  @State private var image = Image(systemName: "person")
+  @State private var image = Image(systemName: "person.circle")
   @State private var text = "Sima Nerush"
   
   var body: some View {
     NavigationStack {
       VStack {
         NavigationLink {
-          ProfileDetailView(text: $text)
+          ProfileDetailView(text: $text, image: $image)
         } label: {
           HStack {
             image
+              .resizable()
+              .scaledToFill()
+              .frame(width: 70, height: 70)
+              .clipShape(Circle())
+              .padding()
             Text(text)
           }
         }
